f2f7e72a78d62ea33a1e8d2752459293d9c3c95b
GersonSurfer <gerson.enriquez@flowing.it>
2022-01-24 08:28:44 +0100

    Implement Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,17 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  height: 72px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
